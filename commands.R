651e552dcf12d685d76c699d7724a034e7830a12
Stephen MacDonald <steviemac180@gmail.com>
2017-11-21 14:00:56 +0000

    started the second post

---
+++ commands.R
@@ -0,0 +1,17 @@
+### Post one
+
+library(tidyverse)
+
+df.prem16_17 <- read_csv(file = "E0.csv")
+
+class(df.prem16_17)
+
+View(df.prem16_17)
+
+dim(df.prem16_17)
+
+names(df.prem16_17)
+
+head(df.prem16_17)
+
+tail(df.prem16_17)
---
+++ public/post/dataload.R
@@ -0,0 +1,3 @@
+## @knitr dataload
+library(tidyverse)
+df.prem16_17 <- read_csv(file = "E0.csv")
